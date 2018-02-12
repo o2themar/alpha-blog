2e6d54e02c7f9e29eca4a68912d91ec7f60fe535
Omar Ali <o2themar6@gmail.com>
2018-02-11 21:39:48 -0500

    Compling assests for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.serve_static_files = true 
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -30,7 +29,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
